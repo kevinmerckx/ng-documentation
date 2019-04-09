3f92a068bc90628f3d12bd38a124485e4c19f213
Kevin Merckx <merckx-git@interfacewerk.de>
2019-04-09 08:30:56 +0200

    forgotten copy button component files

---
+++ projects/ng-documentation/src/lib/core/copy-button/copy-button.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'docs-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.sass']
+})
+export class CopyButtonComponent {
+  @Input() element: HTMLElement;
+
+  copied = false;
+
+  private copiedTimeout: any;
+
+  /**
+   * @ignore
+   */
+  copy() {
+    this.copyToClipboard(this.element.innerText);
+    this.copied = true;
+    clearTimeout(this.copiedTimeout);
+    this.copiedTimeout = setTimeout(() => this.copied = false, 3000);
+  }
+
+  /**
+   * @ignore
+   */
+  private copyToClipboard(str: string) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+
+}
