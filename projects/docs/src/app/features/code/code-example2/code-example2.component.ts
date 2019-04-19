c22343c73f3f6bda3121f56d92be79e4ee65a8cb
Kevin Merckx <merckx-git@interfacewerk.de>
2019-04-19 12:23:58 +0200

    feat(code): enable content projection

---
+++ projects/docs/src/app/features/code/code-example2/code-example2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-example2',
+  templateUrl: './code-example2.component.html',
+  styleUrls: ['./code-example2.component.sass']
+})
+export class CodeExample2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
